3d50028c5284fcf09be58f12600eaa732ea23f9f
Victor Mello <mello@inspiron.localdomain>
2025-01-25 17:45:48 -0300

    Add the task for module 10

---
+++ mod10/StudentAssessmentSystem/src/Main.java
@@ -0,0 +1,52 @@
+import enumerator.ApprovalStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println("Student grade assessment system");
+        System.out.println("==================================");
+
+        Scanner sc = new Scanner(System.in);
+        List<Double> studentGrades = new ArrayList<>();
+        double gradeAverage;
+        ApprovalStatus approvalStatus;
+
+        for(int index = 0; index < 4; index++){
+            System.out.printf("Enter Grade %d:\n", index + 1);
+            studentGrades.add(sc.nextDouble());
+        }
+
+        sc.close();
+
+        int count = 1;
+
+        for(double grade: studentGrades){
+            System.out.println("------------");
+            System.out.printf("Grade %d: %.2f \n", count, grade);
+            count++;
+        }
+
+        gradeAverage = studentGrades.stream().mapToDouble(grade -> grade).average().orElse(0.0);
+
+        System.out.println(" ");
+
+        if (gradeAverage >= 7){
+            approvalStatus = ApprovalStatus.APPROVED;
+        }
+        else if(gradeAverage >= 5){
+            approvalStatus = ApprovalStatus.RECOVERY;
+
+        }
+        else {
+            approvalStatus = ApprovalStatus.FAILED;
+        }
+
+        System.out.println("Average: " + gradeAverage);
+        System.out.println("Status: " + approvalStatus);
+
+    }
+}
---
+++ mod10/StudentAssessmentSystem/src/enumerator/ApprovalStatus.java
@@ -0,0 +1,7 @@
+package enumerator;
+
+public enum ApprovalStatus {
+    APPROVED,
+    RECOVERY,
+    FAILED
+}
