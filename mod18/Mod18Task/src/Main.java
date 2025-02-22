359375797e953a1f15a0fb1f951f7edef0b25d2b
Victor Mello <mellodevictorgabriel@gmail.com>
2025-02-22 13:56:33 -0300

    add task related to module 18

---
+++ mod18/Mod18Task/src/Main.java
@@ -0,0 +1,11 @@
+import entities.Customer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Customer customer = new Customer();
+
+        System.out.println(customer);
+
+    }
+}
---
+++ mod18/Mod18Task/src/annotation/Table.java
@@ -0,0 +1,11 @@
+package annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Table {
+    String name();
+}
+
